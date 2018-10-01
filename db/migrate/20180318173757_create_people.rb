8aa98eac9f2435d381d89db4e8dbcd8f2e1ad18d
Lourdes <montalvo.lourdes@pucp.edu.pe>
2018-10-01 11:24:27 -0500

    develop project of thesis

---
+++ db/migrate/20180318173757_create_people.rb
@@ -0,0 +1,10 @@
+class CreatePeople < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.string :dni
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create!(dni:'70187403', name:'Lourdes Giannina')
+Person.create!(dni:'07617231', name:'Giannina del Pilar')
