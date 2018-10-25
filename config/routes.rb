aab470cf8e795275f7ae7ca5dcba1da352a0292f
Lourdes <montalvo.lourdes@pucp.edu.pe>
2018-10-25 11:59:20 -0500

    try to create login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :usuarios
+    devise_scope :usuario do
+    get "signup", to: "devise/registrations#new"
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+    authenticated :usuario do
+      root 'dashboard#index'
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new'
+    end
+  end
+
   resources :menus
   resources :pathologies
   resources :people
   root 'people#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
