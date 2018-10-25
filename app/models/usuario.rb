17b90c1b6c66710d502005ef51b98454547e2828
Lourdes <montalvo.lourdes@pucp.edu.pe>
2018-10-25 13:35:13 -0500

    login almost done

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,40 +1,11 @@
 class Usuario < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :rol, required: false
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(nombre_usuario) = :value OR lower(correo_electronico) = :value", { :value => login.downcase }]).first
-    else
-      current_user = where(conditions).first
-      current_user.active? ? current_user : false
-    end
-  end
-
-  def omniauth_values auth
-    self.correo_electronico = auth.info.email
-    self.contrasenha = Devise.friendly_token[0,20]
-    self.provider = auth.provider
-    self.uid = auth.uid
-    self.nombres = auth.info.name
-    self.oauth_token = auth.credentials.token
-    self.oauth_expires_at = Time.at(auth.credentials.expires_at)
-  end
-
-  def self.from_omniauth(auth)
-    user = Usuario.find_by_constrasenha auth.info.email
-    if user
-      if !user.oauth_token
-        user.omniauth_values auth
-        user.save
-      end
-      return user
-    end
-    return nil
-  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181025013157) do
+ActiveRecord::Schema.define(version: 20181025173156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,18 @@ ActiveRecord::Schema.define(version: 20181025013157) do
     t.index ["menu_id"], name: "index_rols_on_menu_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   create_table "usuarios", force: :cascade do |t|
     t.text "nombres"
     t.text "apellidos"
@@ -66,6 +78,13 @@ ActiveRecord::Schema.define(version: 20181025013157) do
     t.bigint "rol_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_usuarios_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
     t.index ["rol_id"], name: "index_usuarios_on_rol_id"
   end
 
